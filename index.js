0d7bc9a14ab3f2f35da4090c7dfe4efa284a2b2f
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-01-30 19:56:58 +0500

    code

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-import expreress from 'express'
-const app =  expreress()
+import express from 'express'
+import { MongoClient } from 'mongodb'
+const app =  express()
 
 const port = 3200
+
+const dbneam = 'school'
+const url = 'mongodb+srv://muhabbatali:myworldisstart$$$@cluster0.fepov5t.mongodb.net/'
+
+const client = new MongoClient(url)
 
 
 app.get('/', (req,res) => {
