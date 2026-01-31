23c4ce44a8d19513f9bf1ef8dcba9904c6d566dc
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-01-31 12:25:08 +0500

    newcode

--- index.js
+++ index.js
@@ -2,27 +2,30 @@ import express from 'express'
 import { MongoClient } from 'mongodb'
 const app = express()
 
-
 const dbneam = 'school'
 const url = 'mongodb+srv://muhabbatali:myworldisstart$$$@cluster0.fepov5t.mongodb.net/'
 
-
-const client = new  MongoClient(url)
-
+const client = new MongoClient(url)
 
 async function dnFunction() {
-   await client.connect()
-   const db = client.db(dbneam)
-const collectionneam = db.collection('student')
+  await client.connect()
+  const db = client.db(dbneam)
+  const collectionneam = db.collection('student')
 
-   const result = await collectionneam.find()
-   console.log(result)
+  // ✅ sirf Muhammad Ali save karna
+  await collectionneam.insertOne({
+    name: "Muhammad Ali"
+  })
+
+  const result = await collectionneam.find().toArray()
+  console.log(result)
 }
 dnFunction()
 
-app.get('/' ,(req, res) => {
-    res.end("papan")
+app.get('/', (req, res) => {
+  res.end("papan")
 })
+
 app.listen(3200, () => {
   console.log("http://localhost:3200")
 })
