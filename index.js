8e09c2ae8b19b5e6686bec43239d784b46f94376
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-02-01 11:37:22 +0500

    code

--- index.js
+++ index.js
@@ -1,56 +1,83 @@
+// import express from 'express'
+// import dotenv from 'dotenv'
+// import mongoose from 'mongoose'
+// import path from 'path'
+
+// dotenv.config()
+// const app = express()
+
+// // middlewares
+// app.use(express.json())
+// app.use(express.urlencoded({ extended: true }))
+
+// // ejs
+// app.set('view engine', 'ejs')
+// app.set('views', path.join(process.cwd(), 'views'))
+
+// // mongo connect
+// mongoose.connect(process.env.MONGO_URL)
+//   .then(() => console.log('MongoDB Connected'))
+//   .catch(err => console.log(err))
+
+// // schema + model
+// const studentSchema = new mongoose.Schema({
+//   name: String,
+//   fatherName: String
+// })
+
+// const Student = mongoose.model('Student', studentSchema)
+
+// // home (form)
+// app.get('/', (req, res) => {
+//   res.render('index')
+// })
+
+// // add data (POST)
+// app.post('/add', async (req, res) => {
+//   const { name, fatherName } = req.body
+//   await Student.create({ name, fatherName })
+//   res.redirect('/students')
+// })
+
+// // get all data
+// app.get('/students', async (req, res) => {
+//   const students = await Student.find()
+//   res.render('students', { students })
+// })
+
+// // delete data
+// app.post('/delete/:id', async (req, res) => {
+//   await Student.findByIdAndDelete(req.params.id)
+//   res.redirect('/students')
+// })
+
+// app.listen(3200, () => {
+//   console.log('http://localhost:3200')
+// })
+
+
+// complete code commit
+
 import express from 'express'
-import dotenv from 'dotenv'
-import mongoose from 'mongoose'
-import path from 'path'
+ const app = express()
 
-dotenv.config()
-const app = express()
 
-// middlewares
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
 
-// ejs
-app.set('view engine', 'ejs')
-app.set('views', path.join(process.cwd(), 'views'))
 
-// mongo connect
-mongoose.connect(process.env.MONGO_URL)
-  .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log(err))
+ app.get('/', (req, res) => {
+   res.end("papan")
+ })
+ app.get('/product', (req, res) => {
+   res.end("papan")
+ })
+ app.get('/servese', (req, res) => {
+   res.end("papan")
+ })
+ app.get('/deta', (req, res) => {
+   res.end("papan")
+ })
 
-// schema + model
-const studentSchema = new mongoose.Schema({
-  name: String,
-  fatherName: String
-})
 
-const Student = mongoose.model('Student', studentSchema)
-
-// home (form)
-app.get('/', (req, res) => {
-  res.render('index')
-})
-
-// add data (POST)
-app.post('/add', async (req, res) => {
-  const { name, fatherName } = req.body
-  await Student.create({ name, fatherName })
-  res.redirect('/students')
-})
-
-// get all data
-app.get('/students', async (req, res) => {
-  const students = await Student.find()
-  res.render('students', { students })
-})
-
-// delete data
-app.post('/delete/:id', async (req, res) => {
-  await Student.findByIdAndDelete(req.params.id)
-  res.redirect('/students')
-})
-
-app.listen(3200, () => {
-  console.log('http://localhost:3200')
-})
+  app.listen(3200, () => {
+   console.log('http://localhost:3200')
+ })
