2fe6a201ec42b2b35b7f51ec357d9762b8629305
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-02-04 20:02:50 +0500

    new code

--- index.js
+++ index.js
@@ -1,28 +1,14 @@
 import express from "express";
 import userrouter from './routes/userRouter.js'
+ import hostrouter from './routes/hostrouter.js'
+
 
 const app = express();
 app.use(express.urlencoded({extended: true}));
 
 
-
 app.use(userrouter)
-app.get("/add-home", (req, res) => {
-  res.send(`<h1>rigester your  home here  </h1>
-      <form action="/add-home" method="post">
-     <input type="text" name="houseneam" placeholder="enter your house neam">
-     <input type="submit">
-  </form>
-    `);
-});
-
-
-app.post("/add-home", (req, res) => {
-  console.log(req.body);
-  res.send(`<h1>rigester your  home successful </h1>
-    <a href="/">add home</a>
-    `);
-});
+app.use(hostrouter)
 
 
 
