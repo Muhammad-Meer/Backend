f2c9e55dbd2930d9d1bb6883d5c0750e92dbaa15
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-02-04 17:20:49 +0500

    new code

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
-import e from('express')
+import express from "express";
 
-const storeroutrt = e()
+const app = express();
 
-const homecontroler = require('./scheema/studentscheema')
+app.get("/", (req, res) => {
+  console.log(req.url, req.method);
+  res.send(`<h1> welcome to airbnb</h1>
+    <a href="/add-home">add home</a>
+    `);
+});
+
+app.get("/add-home", (req, res) => {
+  console.log(req.url, req.method);
+  res.send(`<h1>rigester your  home here  </h1>
+      <form action="/add-home" method="post">
+     <input type="text" name="houseneam" placeholder="enter your house neam">
+     <input type="submit">
+  </form>
+    `);
+});
 
 
-storeroutrt.get('/',homecontroler.getindex)
+app.post("/add-home", (req, res) => {
+  console.log(req.url, req.method);
+  res.send(`<h1>rigester your  home successful </h1>
+      <form action="/add-home" method="post">
+     <input type="text" name="houseneam" placeholder="enter your house neam">
+     <input type="submit">
+  </form>
+    `);
+});
+
+
+
+
+const port = 3200;
+app.listen(port, () => {
+  console.log("http://localhost:" + port);
+});
--- model/studentModel.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-import studentscheema from "../scheema/studentscheema.js";
-
-const scheemamodel = mongoose.model('student', studentscheema)
-export default scheemamodel
