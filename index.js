3d2c08afb05984d7e203d48817255e1b9f6b3183
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-02-04 20:20:52 +0500

    new code

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 import express from "express";
+
+
 import userrouter from './routes/userRouter.js'
- import hostrouter from './routes/hostrouter.js'
+import hostrouter from './routes/hostrouter.js'
 
 
 const app = express();
+
+
 app.use(express.urlencoded({extended: true}));
-
-
 app.use(userrouter)
 app.use(hostrouter)
-
-
 
 
 const port = 3200;
--- routes/hostrouter.js
+++ routes/hostrouter.js
@@ -1,21 +1,21 @@
 import express from 'express'
 const hostrouter = express.Router()
 
-hostrouter.get("/add-home", (req, res) => {
+hostrouter.get("/host/add-home", (req, res) => {
   res.send(`
     <h1>register your home here</h1>
-    <form action="/add-home" method="post">
+    <form action="/host/add-home" method="post">
       <input type="text" name="housename" placeholder="enter your house name">
       <input type="submit">
     </form>
   `)
 })
 
-hostrouter.post("/add-home", (req, res) => {
+hostrouter.post("/host/add-home", (req, res) => {
   console.log(req.body)
   res.send(`
     <h1>register your home successful</h1>
-    <a href="/add-home">add home</a>
+    <a href="/host/add-home>add home</a>
   `)
 })
 
