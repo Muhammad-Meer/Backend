bc58e7d9f1fe5e49f602cf6993824fe663407b30
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-02-07 18:18:52 +0500

    new code

--- routes/hostrouter.js
+++ routes/hostrouter.js
@@ -3,4 +3,8 @@ const path = require('path')
 const express = require('express')
 const hostrouter = express.Router() 
 
+hostrouter.get("/add-home", (req, res) => {
+  res.sendFile(path.join(__dirname, '../', 'views', 'home.html'));
+});
+
 const rootdir = require("/")
